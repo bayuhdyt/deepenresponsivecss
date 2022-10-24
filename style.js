4987c26b67fa409363f311896c2fcf7062aa0f8a
bayuhdyt <bayuhendriyanto918@gmail.com>
2022-10-24 22:33:06 +0700

    first commit

---
+++ style.js
@@ -0,0 +1,11 @@
+const linkedin = () => {
+  window.location.href = "https://www.linkedin.com/in/bayu-hendriyanto-160148170/";
+};
+
+const instagram = () => {
+  window.location.href = "https://www.instagram.com/bayuhendriyanto/";
+};
+
+const google = () => {
+  window.location.href = "https://www.google.com";
+};
